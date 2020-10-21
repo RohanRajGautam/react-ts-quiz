f6e92d5c83e7fd0a0feadea4cb64a01e7770db2a
rohan <rohanrajgautam@gmail.com>
2020-10-21 20:31:19 +0545

    added additional logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,43 @@ import {fetchQuizQuestions} from './API'
 //components
 import QuestionCard from './components/QuestionCard';
 //types
-import {Difficulty} from './API'
+import {QuestionState, Difficulty} from './API'
 
+type AnswerObject = {
+  question: string,
+  answer: string,
+  correct: boolean,
+  correctAnswer: string,
+}
 
 const TOTAL_QUESTIONS = 10;
 
 function App() {
 
 const [loading, setLoading] = useState(false);
-const [questions, setQuestions] = useState([]);
-const [userAnswers, setUserAnswers] = useState([])
+const [questions, setQuestions] = useState<QuestionState[]>([]);
+const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
 const [number, setNumber] = useState(0)
 const [score, setScore] = useState(0)
 const [gameOver, setGameOver] = useState(true)
 
-console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+console.log(questions);
 
 
   const startTrivia = async () => {
+    setLoading(true)
+    setGameOver(false)
 
+    const newQuestion = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    )
+
+    setQuestions(newQuestion)
+    setScore(0)
+    setUserAnswers([])
+    setNumber(0)
+    setLoading(false)
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -36,9 +54,11 @@ console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
   return (
     <div className="App">
       <h1>RRG Quiz App</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
       <button className="start" onClick={startTrivia}>Start</button>
-      <p className="score">Score: </p>
-      <p>Your Questions are loading...</p>
+      ) : null }
+      { !gameOver && <p className="score">Score: </p> }
+      { loading && <p>Questions Loading ...</p> }
       {/* <QuestionCard 
         questionNm={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
