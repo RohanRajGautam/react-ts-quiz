20010dca6a787f9668ccbe57cfd93487af5b8a3e
rohan <rohanrajgautam@gmail.com>
2020-10-21 23:22:03 +0545

    Updates

---
+++ precache-manifest.798044d35567ba36db96dab97af1872a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7645ab06e9512603e99cfcbdc20bc0",
+    "url": "/react-ts-quiz/index.html"
+  },
+  {
+    "revision": "148b9e3155b3c141aa3d",
+    "url": "/react-ts-quiz/static/js/2.cdf1552d.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/react-ts-quiz/static/js/2.cdf1552d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71d7a2f599a4836ccf09",
+    "url": "/react-ts-quiz/static/js/main.078f3a89.chunk.js"
+  },
+  {
+    "revision": "e250c5d2c1a165f227eb",
+    "url": "/react-ts-quiz/static/js/runtime-main.f8725bbe.js"
+  },
+  {
+    "revision": "c88ad10874208632af34a7fd152490e4",
+    "url": "/react-ts-quiz/static/media/quiz-app.c88ad108.jpg"
+  }
+]);
